dd7e6d1aa0238b39a90bc5995df9450d0831588d
MrTaquitous <marcosjpincemin@gmail.com>
2026-03-13 15:07:04 +0100

    Integrated new functions to the class series and created the enum genre to list the different genres of each series/films

---
+++ SeriesList/src/enumPack/genre.java
@@ -0,0 +1,50 @@
+package enumPack;
+
+/**
+ *Ennumeration file made to differentiate the genre of each film/series
+ * Contains every main genre
+ *
+ * @author MrTaquitous
+ * @version 1.0
+ * @since 13/03/2026
+ */
+
+/*
+NaN = For when you don't know what genre to put.
+Action = Associated with particular types of spectacle (e.g., explosions, chases, combat)
+Adventure = Implies a narrative that is defined by a journey (often including some form of pursuit) and is usually located within a fantasy or exoticized setting.
+Animated = A film medium in which the film's images are primarily created by computer or hand and the characters are voiced by actors.
+Comedy = Defined by events that are primarily intended to make the audience laugh.
+Drama = Focused on emotions and defined by conflict, often looking to reality rather than sensationalism.
+Fantasy = Films defined by situations that transcend natural laws and/or by settings inside a fictional universe, with narratives that are often inspired by or involve human myths.
+Historical = Films that either provide more-or-less accurate representations of historical accounts or depict fictional narratives placed inside an accurate depiction of a historical setting.
+Horror = Films that seek to elicit fear or disgust in the audience for entertainment purposes.
+Melodrama = A genre, mode, style or sensibility characterized by an emphasis on intense and exaggerated emotions and heightened dramatic situations.
+Musical = A genre in which songs performed by the characters are interwoven into the narrative, sometimes accompanied by dancing.
+Noir = A genre of stylish crime dramas particularly popular during the 1940s and '50s.
+Pornographic = Pornographic films are typically categorized as either softcore or hardcore pornography. In general, softcore pornography is pornography that does not depict explicit sexual activity, sexual penetration or extreme fetishism.
+Romance = Characterized by an emphasis on passion, emotion, and the affectionate romantic involvement of the main characters, with romantic love or the search for it typically being the primary focus.
+Science = Films are defined by a combination of imaginative speculation and a scientific or technological premise, making use of the changes and trajectory of technology and science.
+Thriller = Films that evoke excitement and suspense in the audience.
+Western = A genre in which films are set in the American West during the 19th century.
+ */
+
+public enum Genre{
+    NaN,
+    Action,
+    Adventure,
+    Animated,
+    Comedy,
+    Drama,
+    Fantasy,
+    Historical,
+    Horror,
+    Melodrama,
+    Musical,
+    Noir,
+    Pornographic,
+    Romance,
+    Science,
+    Thriller,
+    Western,
+}
