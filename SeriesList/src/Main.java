94441d7037c4fcb86be9f29d3291596dee92de85
Cyril <akaalldestrima@gmail.com>
2026-03-13 16:51:37 +0100

    FEAT : début boucle main

--- SeriesList/src/Main.java
+++ SeriesList/src/Main.java
@@ -1,15 +1,53 @@
+import classes.Series;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        String reponse;
+        ArrayList<Series> series = new ArrayList<>();
+        do {
+            System.out.print("Que voulez-vous faire ?\nQuitter : q\nVoir Series : 1\nAjouter une série : 2\n");
+            reponse = scanner.nextLine().trim().toLowerCase();
+            if(reponse.equals("1")) {
+                for (Series serie : series){
+                    serie.displayInformation();
+                }
+            }
+            if(reponse.equals("2")) {
+                //    public Series(String name, int Seasons, LocalDate releaseDate, LocalDate endDate){
+                System.out.print("Quel est le nom de votre Série ?\n");
+                String name = scanner.nextLine();
+                System.out.print("Sortie en quelle année ?\n");
+                int year = Integer.parseInt(scanner.nextLine());
+                System.out.print("Sortie quel mois ?\n");
+                int month = Integer.parseInt(scanner.nextLine());
+                System.out.print("Sortie quel jour ?\n");
+                int day = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+                System.out.print("La série est-t-elle finie ? (o/n)\n");
+                String response = scanner.nextLine();
+                do {
+                    if(response.equals("o")) {
+                        System.out.print("Quelle date de fin année ?\n");
+                        int yearEnd = Integer.parseInt(scanner.nextLine());
+                        System.out.print("Date de fin : mois ?\n");
+                        int monthEnd = Integer.parseInt(scanner.nextLine());
+                        System.out.print("date de fin jour ?\n");
+                        int dayEnd = Integer.parseInt(scanner.nextLine());
+                        Series series1 = new Series(name, LocalDate.of(year,month,day),LocalDate.of(yearEnd,monthEnd,dayEnd));
+                        series.add(series1);
+                    } else if(response.equals("n")) {
+                        Series series1 = new Series(name, LocalDate.of(year,month,day));
+                        series.add(series1);
+                    }
+                } while (!response.equals("o") && !response.equals("n"));
+            }
+        } while (!reponse.equals("q"));
     }
 }
--- SeriesList/src/classes/Series.java
+++ SeriesList/src/classes/Series.java
@@ -2,20 +2,29 @@ package classes;
 
 import enumPack.Genre;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Series {
     String name;
-    int seasons;
+    ArrayList<Season> seasons;
     Rating rating;
     LocalDate releaseDate;
     LocalDate endDate;
     Genre genre;
 
-    public Series(String name, int Seasons, LocalDate releaseDate, LocalDate endDate){
+    public Series(String name, LocalDate releaseDate, LocalDate endDate){
         this.setName(name);
-        this.setSeasons(Seasons);
+        this.seasons = new ArrayList<Season>();
         this.setReleasedate(releaseDate);
         this.setEndDate(endDate);
+        this.setGenre(Genre.NaN);
+        this.rating = new Rating();
+    }
+
+    public Series(String name, LocalDate releaseDate){
+        this.setName(name);
+        this.seasons = new ArrayList<Season>();
+        this.setReleasedate(releaseDate);
         this.setGenre(Genre.NaN);
         this.rating = new Rating();
     }
@@ -23,7 +32,7 @@ public class Series {
     public void setName(String name) {
         this.name = name;
     }
-    public void setSeasons(int seasons){
+    public void setSeasons(ArrayList<Season> seasons){
         this.seasons = seasons;
     }
     public void setReleasedate(LocalDate releaseDate){
@@ -38,7 +47,15 @@ public class Series {
     public String getName(){
         return name;
     }
-    public int getSeasons(){return seasons;}
+
+    public int getNumberSeasons(){
+        int nb = 0;
+        for (Season season : this.seasons) {
+            nb+=1;
+        }
+        return nb;
+    }
+    public ArrayList<Season> getSeasons(){return seasons;}
     public LocalDate getReleaseDate(){return releaseDate;}
     public LocalDate getEndDate(){return endDate;}
     public Genre getGenre(){return genre;}
@@ -48,7 +65,7 @@ public class Series {
         StringBuilder sb = new StringBuilder();
         sb.append(this.getName());
         sb.append(" | # of seasons: ");
-        sb.append(this.getSeasons());
+        sb.append(this.getNumberSeasons());
         sb.append(" | Rating: ");
         sb.append(this.rating.printRatingTotalToString());
         sb.append("\n\t");
