9046b4f07cada1954d25c54e2042a0aec0ff42d7
MrTaquitous <marcosjpincemin@gmail.com>
2026-03-18 09:47:37 +0100

    Changed language of main to english (idc that I live in France, as soon as I read a line of code that is not english I'm changing it bitch)

--- SeriesList/src/Main.java
+++ SeriesList/src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         String reponse;
         ArrayList<Series> series = new ArrayList<>();
         do {
-            System.out.print("Que voulez-vous faire ?\nQuitter : q\nVoir Series : 1\nAjouter une série : 2\n");
+            System.out.print("What do you want to do?\nQuit: q\nList series: 1\nAdd a series: 2\n");
             reponse = scanner.nextLine().trim().toLowerCase();
             if(reponse.equals("1")) {
                 for (Series serie : series){
@@ -32,24 +32,24 @@ public class Main {
             }
             if(reponse.equals("2")) {
                 //    public Series(String name, int Seasons, LocalDate releaseDate, LocalDate endDate){
-                System.out.print("Quel est le nom de votre Série ?\n");
+                System.out.print("What's the name of the series\n");
                 String name = scanner.nextLine();
-                System.out.print("Sortie en quelle année ?\n");
+                System.out.print("What year did it go out?\n");
                 int year = Integer.parseInt(scanner.nextLine());
-                System.out.print("Sortie quel mois ?\n");
+                System.out.print("What month did it go out?\n");
                 int month = Integer.parseInt(scanner.nextLine());
-                System.out.print("Sortie quel jour ?\n");
+                System.out.print("What day did it go out?\n");
                 int day = Integer.parseInt(scanner.nextLine());
 
                 System.out.print("La série est-t-elle finie ? (o/n)\n");
                 String response = scanner.nextLine();
                 do {
-                    if(response.equals("o")) {
-                        System.out.print("Quelle date de fin année ?\n");
+                    if(response.equals("y")) {
+                        System.out.print("What year did it end?\n");
                         int yearEnd = Integer.parseInt(scanner.nextLine());
-                        System.out.print("Date de fin : mois ?\n");
+                        System.out.print("What month did it end?\n");
                         int monthEnd = Integer.parseInt(scanner.nextLine());
-                        System.out.print("date de fin jour ?\n");
+                        System.out.print("What day did it end?\n");
                         int dayEnd = Integer.parseInt(scanner.nextLine());
                         Series series1 = new Series(name, LocalDate.of(year,month,day),LocalDate.of(yearEnd,monthEnd,dayEnd));
                         series.add(series1);
@@ -57,7 +57,7 @@ public class Main {
                         Series series1 = new Series(name, LocalDate.of(year,month,day));
                         series.add(series1);
                     }
-                } while (!response.equals("o") && !response.equals("n"));
+                } while (!response.equals("y") && !response.equals("n"));
             }
         } while (!reponse.equals("q"));
     }
